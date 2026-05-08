37a1fe1f2e024a5baf1432b988b0262f6234ce7b
Anderson <anderson.marco@gmail.com>
2026-05-08 20:14:34 -0300

    new flag to avoid the gcc to use memset and memcpy

--- tests/test_c/build.sh
+++ tests/test_c/build.sh
@@ -8,12 +8,14 @@ riscv64-unknown-elf-gcc \
 -fdata-sections \
 -ffunction-sections \
 -fno-asynchronous-unwind-tables \
+-fno-tree-loop-distribute-patterns \
 -fno-unwind-tables \
 -fno-exceptions \
 -nostdlib \
 -Wl,--gc-sections \
 -T linker.ld \
 startup.S $1 \
--o firmware.elf
+-lgcc \
+-o firmware.elf 
 
 riscv64-unknown-elf-objcopy -O binary firmware.elf firmware.bin
